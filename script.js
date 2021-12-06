b09fe04e41dcc592c6c5411228381c0f40e8d214
istroxpro <istrodantir@icloud.com>
2021-12-06 16:45:17 +0700

    lesson06 homework

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
-let num = Math.floor(Math.random() * 98) + 2
-let result = false;
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-};
+'use strict';
 
-while (!result) {
-    let answer = prompt('Угадай число от 1 до 100');
-    if (answer === null) {
-        alert('Игра окончена!')
-        break;
-    } else if (answer > num) {
-        alert('Загаданное число меньше');
-    } else if (answer < num) {
-        alert('Загаданное число больше');
-    } else if (answer = num) {
-        confirm('Поздравляю, вы выйграли!!!');
-        result = true;
-        break;
-    }
-    while (!isNumber(num)) {
-        alert('Введи число!!!');
+function letsplay() {
+    let randomNumber = Math.floor(Math.random() * 98) + 2;
+    console.log('randomNumber = ' + typeof randomNumber + ' ' + randomNumber);
+    let counter = 10;
+    alert('У тебя 10 попыток');
+
+    let askUserNumber = function () {
+        let userNumber = prompt('Угадай число от 1 до 100');
+
+        let convertToNumber = +userNumber;
+
+        let isNumber = function (num) {
+            return !isNaN(parseFloat(num)) && isFinite(num);
+        };
+
+        let warnUser = function () {
+            alert('Введи число!');
+            askUserNumber();
+        };
+
+        if (userNumber === null) {
+            alert('Игра окончена');
+            return;
+        } else if (!isNumber(userNumber)) {
+            alert('Введи число!');
+            warnUser();
+        }
+
+        let compareUserNumber = function () {
+            let compareUserNumberConfirm;
+            counter--;
+            let checkCounterConfirm;
+            if (counter <= 0) {
+                checkCounterConfirm = confirm('Попытки закончились, хотите сыграть еще?');
+                if (checkCounterConfirm === true) {
+                    return letsplay();
+                } else {
+                    return false;
+                }
+            }
+            if (convertToNumber > randomNumber) {
+                alert('Загаданное число меньше, осталось ' + counter + ' попыток');
+                // checkCounter();
+
+            } else if (convertToNumber < randomNumber) {
+                alert('Загаданное число больше, осталось ' + counter + ' попыток');
+                // checkCounter();
+
+            } else if (convertToNumber === randomNumber) {
+                compareUserNumberConfirm = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+                if (compareUserNumberConfirm === true) {
+                    return letsplay();
+                } else {
+                    return false;
+                }
+            }
+            askUserNumber();
+
+        };
+        compareUserNumber();
+
+    };
+    askUserNumber();
+}
+let start = function () {
+    let question = confirm('Угадай число от 1 до 100');
+    if (question === true) {
+        letsplay();
     }
 };
+start();
